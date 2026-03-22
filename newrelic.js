a209760a0aa5dca114091b66440c26650e49c244
lazykaizer <siddiquegulfam703@gmail.com>
2026-03-22 22:28:09 +0530

    fix: added local env parsing to prevent crash locally

--- newrelic.js
+++ newrelic.js
@@ -7,14 +7,23 @@
  * 2. Bas 'app_name' (line 12 par) ka naam badal dein.
  */
 
+const fs = require('fs');
+const path = require('path');
+let localKey = '';
+try {
+  const envFile = fs.readFileSync(path.join(__dirname, '.env.local'), 'utf8');
+  const match = envFile.match(/NEW_RELIC_LICENSE_KEY="?([^"\n]+)"?/);
+  if (match) localKey = match[1];
+} catch (e) {}
+
 exports.config = {
   // YEH BADALNA HAI NAYE PROJECT KE LIYE:
-  app_name: ['monodesk-app'], 
-  
-  // YEH HAMESHA SECURE RAHEGA:
-  license_key: process.env.NEW_RELIC_LICENSE_KEY || '', 
+  app_name: ['monodesk-app'],
 
-  
+  // YEH HAMESHA SECURE RAHEGA:
+  license_key: process.env.NEW_RELIC_LICENSE_KEY || localKey || '000000000000000000000000000000000000NRAL',
+
+
   logging: {
     level: 'info'
   },
@@ -24,6 +33,7 @@ exports.config = {
       'request.headers.cookie',
       'request.headers.authorization',
       'request.headers.proxyAuthorization',
+
       'request.headers.setCookie*',
       'request.headers.x*',
       'response.headers.cookie',
